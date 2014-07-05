da89bc8a59f4e579e42b63a9b06b60e2e7b3e9cb
danie_000 <danie_000@192.168.25.39>
2014-07-05 19:28:35 -0300

    Novo converter

---
+++ SISTEMA DE CONTROLE JMC/src/main/java/br/com/conjmc/jsf/converter/DespesaGastosConverter.java
@@ -0,0 +1,26 @@
+package br.com.conjmc.jsf.converter;
+import br.com.conjmc.cadastrobasico.Despesa_Gastos;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.roo.addon.jsf.converter.RooJsfConverter;
+
+@Configurable
+@FacesConverter("despesaGastosConverter")
+@RooJsfConverter(entity = Despesa_Gastos.class)
+public class DespesaGastosConverter implements Converter{
+
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        if (value == null || value.length() == 0) {
+            return null;
+        }
+        Long id = Long.parseLong(value);
+        return Despesa_Gastos.findDespesa_Gastos(id);
+    }
+
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+        return value instanceof Despesa_Gastos ? ((Despesa_Gastos) value).getId().toString() : "";
+    }
+}

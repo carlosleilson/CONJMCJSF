f8a680e410151b73b123b6fd814060b54f8de873
danie_000 <danie_000@192.168.25.39>
2014-07-10 03:58:30 -0300

    Colocando letra minuscula

--- SISTEMA DE CONTROLE JMC/src/main/java/br/com/conjmc/cadastrobasico/Perfil.java
+++ SISTEMA DE CONTROLE JMC/src/main/java/br/com/conjmc/cadastrobasico/Perfil.java
@@ -2,5 +2,20 @@ package br.com.conjmc.cadastrobasico;
 
 public enum Perfil {
 
-    EXCLUSIVO, SUPERVISOR, GERENTE, ATENDENTE
+    EXCLUSIVO("Exclusivo"),
+    SUPERVISOR("Supervisor"),
+    GERENTE("Gerente"),
+    ATENDENTE("Atendente");
+    
+    private String label;
+
+	public String getLabel() {
+		return label;
+	}
+
+	private Perfil (String label) {  
+        this.label = label;  
+    }
+    
+    
 }
